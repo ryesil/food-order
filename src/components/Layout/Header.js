ea76725c8a2ce7c43ae1a03b69df5ead4adf7ccd
ryesil <ramazanysl@gmail.com>
2021-10-14 21:39:10 -0500

    Several components added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,18 @@
 import { Fragment } from "react";
-
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
-import { Button } from "@mui/material";
+import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-
-        <Button size="large" variant="contained" color="secondary">
-          Cart
-        </Button>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="meal" />
       </div>
+      <div></div>
     </Fragment>
   );
 };
